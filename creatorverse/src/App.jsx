3509093d4611a98e9ebcda34ed178414475b4fba
Aadil Qureshi <36612205+aadilq@users.noreply.github.com>
2025-08-05 19:33:06 -0400

    Display content creators on Homepage

--- creatorverse/src/App.jsx
+++ creatorverse/src/App.jsx
@@ -1,13 +1,28 @@
-import { useRoutes } from 'react-router-dom';
+import { useEffect, useState} from 'react';
+import {useRoutes } from 'react-router-dom';
 import ShowCreator from './pages/ShowCreators';
 import ViewCreator from './pages/ViewCreator';
 import AddCreator from './pages/AddCreator';
 import EditCreator from './pages/EditCreator';
-
+import { supabase } from './client.js';
+import Card from './components/Card.jsx';
 
 
 function App() {
+  const [creators, setCreators] = useState([]);
+
+  useEffect(()=> {
+    getCreators();
+  }, []);
+
+  async function getCreators () {
+    const { data } = await supabase.from("creators").select()
+    setCreators(data);
+  }
     const routes = useRoutes([
+      {
+        path: "/", 
+        element: <Card creators={creators} /> || "No Creators Yet 😞"},
       {path:"/showCreator", element: <ShowCreator />}, 
       {path:"/viewCreator", element: <ViewCreator />},
       {path:"/addCreator", element: <AddCreator />},
