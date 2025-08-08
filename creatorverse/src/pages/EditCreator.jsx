13db7fbcc259fae0b7712a88361d7eefca4133f7
Aadil Qureshi <36612205+aadilq@users.noreply.github.com>
2025-08-08 14:59:07 -0400

    Create form to edit content creator

--- creatorverse/src/pages/EditCreator.jsx
+++ creatorverse/src/pages/EditCreator.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { supabase } from "../client"
+import { useParams } from 'react-router-dom';
 
 
 function EditCreator(){
@@ -41,6 +42,46 @@ function EditCreator(){
             fetchContentCreator()
         }
     }, [id])
+
+    if(loading){return <p>Loading Profile</p>}
+    if(error){return <p>'Error', {error}</p>}
+    return(
+        <form>
+            <label>
+                Name:
+                <input
+                type="text"
+                value={formData.name}
+                onChange={(e) => setFormData({...formData, name: e.target.value})}
+                />
+            </label>
+            <label>
+                URL:
+                <input
+                type="text"
+                value={formData.url}
+                onChange={(e) => setFormData({...formData, url: e.target.value})}
+                />
+            </label>
+            <label>
+            Description:
+                <input
+                type="text"
+                value={formData.description}
+                onChange={(e) => setFormData({...formData, description: e.target.value})}
+                />
+            </label>
+            <label>
+            Image URL:
+                <input
+                type="text"
+                value={formData.imageURL}
+                onChange={(e) => setFormData({...formData, imageURL: e.target.value})}
+                />
+            </label>
+            <button className="rounded hover:rounded-lg">Save Changes</button>
+        </form>
+    )
 
 };
 export default EditCreator
