c5184150e64b58d31cef20e51374910c5449adaf
Aadil Qureshi <36612205+aadilq@users.noreply.github.com>
2025-08-08 14:26:52 -0400

    Fix Card Component to properly show data

--- creatorverse/src/components/Card.jsx
+++ creatorverse/src/components/Card.jsx
@@ -4,11 +4,14 @@ import ReactDOM from "react-dom/client";
 
 function Card (props){
     return(
-        <div>
-            <h1>{props.name}</h1>
-            <h2>{props.url}</h2>
-            <p>{props.description}</p>
-            <img src = {props.imgURL} alt={props.name || "Card Image"} />
+        <div className="block p-6 w-fit
+            bg-blue-900 rounded-lg 
+            border border-grey-200 shadow-md">
+            <h1>Name</h1>
+            <h1 className="text-3xl font-bold">{props.creator.name}</h1>
+            <h2>{props.creator.url}</h2>
+            <p>{props.creator.description}</p>
+            <img src = {props.creator.imgURL} alt={props.creator.name || "Card Image"} />
         </div>
     )
 }
