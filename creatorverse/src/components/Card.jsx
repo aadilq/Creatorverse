a3bd05959ff8d4908c22febde192faba11c73e0c
Aadil Qureshi <36612205+aadilq@users.noreply.github.com>
2025-08-11 08:11:58 -0400

    Modify navigation link in card component

--- creatorverse/src/components/Card.jsx
+++ creatorverse/src/components/Card.jsx
@@ -1,17 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import EditCreator from "../pages/EditCreator";
+import ViewCreator from "../pages/ViewCreator";
+import { useNavigate } from "react-router-dom";
 
 
 function Card (props){
+    const navigate = useNavigate();
+    
+    const navigateToEditCreator = () =>{
+        navigate(`/editCreator/${props.creator.name}`)
+      };
+    const navigateToViewCreator = () =>{
+        navigate(`/viewCreator/${props.creator.name}`)
+      };
+
+    
     return(
-        <div className="block p-6 w-fit
-            bg-blue-900 rounded-lg 
-            border border-grey-200 shadow-md">
-            <h1>Name</h1>
-            <h1 className="text-3xl font-bold">{props.creator.name}</h1>
-            <h2>{props.creator.url}</h2>
-            <p>{props.creator.description}</p>
+        <div className="block p-6 w-fit bg-blue-900 rounded-lg border border-grey-200 shadow-md py-15 m-10 mx-auto">
+            <label className="pb-10">
+            <h1 className="text-3xl font-bold pb-5">{props.creator.name}</h1>
+            </label>
+            <label>
+            <h2 className="pb-5">{props.creator.url}</h2>
+            </label>
+            <p className="font-semibold pb-5">{props.creator.description}</p>
             <img src = {props.creator.imgURL} alt={props.creator.name || "Card Image"} />
+            <div className="space-x-4">
+            <button onClick={navigateToEditCreator}>Edit Creator</button>
+            <button onClick={navigateToViewCreator}>View Creator</button>
+            </div>
         </div>
     )
 }
