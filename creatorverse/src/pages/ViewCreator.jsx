3c59ac7d9ac95ffd9dd15cfa5aea92258c59c758
Aadil Qureshi <36612205+aadilq@users.noreply.github.com>
2025-08-10 21:02:52 -0400

    Fix useParams issue

--- creatorverse/src/pages/ViewCreator.jsx
+++ creatorverse/src/pages/ViewCreator.jsx
@@ -2,31 +2,26 @@ import { useEffect, useState } from 'react';
 import { supabase } from '../client.js';
 import { useParams } from 'react-router-dom';
 
-
-
 function ViewCreator(){
-    const [contentCreator, SetContentCreator] = useState([]);
-    const { id } = useParams();
-    
+    const [contentCreator, SetContentCreator] = useState(null);
+    const { creatorID } = useParams();
 
+    
     useEffect(() => {
         async function getContentCreator() {
             const { data } = await supabase.
             from("creators").
             select('*')
-            .eq("id", id)
-            .single()
-            SetContentCreator(data)
+            .eq("name", creatorID)
+            .limit(1)
+
+            SetContentCreator(data[0])
         }
 
-        if(id){
+        if(creatorID){
             getContentCreator()
         }
-    }, [id])
-
-
-
-
+    }, [creatorID])
     if(!contentCreator){
         return(
             <div>
@@ -37,10 +32,10 @@ function ViewCreator(){
 
     return(
         <div>
-            <h1>{contentCreator.name}</h1>
-            <h2>{contentCreator.url}</h2>
-            <p>{contentCreator.description}</p>
-            <p>{contentCreator.imageURL}</p>
+            <h1> <span>Name: </span>{contentCreator.name}</h1>
+            <h2> <span>Social Media URL: </span>{contentCreator.url}</h2>
+            <p> <span>Description: </span>{contentCreator.description}</p>
+            <p> <span>Image URL: </span>{contentCreator.imageURL}</p>
         </div>
     )
     
