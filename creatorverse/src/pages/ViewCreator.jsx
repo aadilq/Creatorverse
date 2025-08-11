4b5068a97dd01670336814d97dfe1897bd8a8fa6
Aadil Qureshi <36612205+aadilq@users.noreply.github.com>
2025-08-10 22:03:41 -0400

    Add edit button on ViewCreator page

--- creatorverse/src/pages/ViewCreator.jsx
+++ creatorverse/src/pages/ViewCreator.jsx
@@ -1,11 +1,17 @@
 import { useEffect, useState } from 'react';
 import { supabase } from '../client.js';
 import { useParams } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 function ViewCreator(){
     const [contentCreator, SetContentCreator] = useState(null);
     const { creatorID } = useParams();
 
+    const navigate = useNavigate();
+
+    const navigateToEditCreator = () =>{
+        navigate(`/editCreator/${creatorID}`)
+      };
     
     useEffect(() => {
         async function getContentCreator() {
@@ -33,9 +39,10 @@ function ViewCreator(){
     return(
         <div>
             <h1> <span>Name: </span>{contentCreator.name}</h1>
-            <h2> <span>Social Media URL: </span>{contentCreator.url}</h2>
-            <p> <span>Description: </span>{contentCreator.description}</p>
-            <p> <span>Image URL: </span>{contentCreator.imageURL}</p>
+            <h2> <span className='font-bold'>Social Media URL: </span>{contentCreator.url}</h2>
+            <p> <span className='font-bold'>Description: </span>{contentCreator.description}</p>
+            <p> <span className='font-bold'>Image URL: </span>{contentCreator.imageURL}</p>
+            <button onClick={navigateToEditCreator}>View Creator</button>
         </div>
     )
     
