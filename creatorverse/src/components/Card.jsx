1424a343776a22cd8ff8b9b26ebef6bb5604cc0e
Aadil Qureshi <36612205+aadilq@users.noreply.github.com>
2025-08-12 16:04:31 -0400

    Fix Card image positioning

--- creatorverse/src/components/Card.jsx
+++ creatorverse/src/components/Card.jsx
@@ -1,49 +1,12 @@
-import React, { useEffect } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import EditCreator from "../pages/EditCreator";
 import ViewCreator from "../pages/ViewCreator";
 import { useNavigate } from "react-router-dom";
-import { supabase } from "../client";
-import { useState } from "react";
+
 
 
 function Card (props){
-  const [signedUrl, setSignedURL] = useState('');
-
-  useEffect(() => {
-    async function getSignedURL () {
-      if(!props.creator.imageURL) return;
-       
-      try {
-        let filePath; 
-        if(props.creator.imageURL.includes('supabase.co')){
-           filePath = props.creator.imageURL.replace(
-            'https://abc123.supabase.co/storage/v1/object/public/creators/', 
-            ''
-          )
-        }
-        else{
-          filePath = props.creator.imageURL
-        }
-       
-        console.log("Using file path for signed URL:", filePath);
-
-        const { data, error} = await supabase.storage.from("creators").createSignedUrl(filePath, 60 * 60);
-
-        if(error){
-          throw error
-        }
-        setSignedURL(data.signedUrl)
-        console.log("Signed URL created:", data.signedUrl);
-      } catch (error) {
-        console.error('Error creating signed URL:', error.message);
-        setSignedURL(props.creator.imageURL)
-      }
-    }
-    getSignedURL()
-  }, [props.creator.imageURL])
-
-
   const navigate = useNavigate();
     
     const navigateToEditCreator = () =>{
@@ -64,7 +27,7 @@ function Card (props){
             <h2 className="pb-5">{props.creator.url}</h2>
             </label>
             <p className="font-semibold pb-5">{props.creator.description}</p>
-            <img src = {signedUrl || props.creator.imageURL} alt={props.creator.name || "Card Image"} />
+            <img className = "mx-auto" src = {props.creator.imageURL} alt={props.creator.name || "Card Image"} />
             <div className="space-x-4">
             <button onClick={navigateToEditCreator}>Edit Creator</button>
             <button onClick={navigateToViewCreator}>View Creator</button>
